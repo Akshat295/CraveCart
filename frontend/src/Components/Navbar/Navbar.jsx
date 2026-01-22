af80abe22ba30549dd32056bc2c065a749c5b995
Akshat295 <akshatjohri55@gmail.com>
2026-01-22 18:33:54 +0530

    done changes in frontend

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,15 +1,20 @@
 import React, { useState, useEffect, useContext } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/frontend_assets/assets'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { StoreContext } from '../../Context/StoreContext';
 const Navbar = ({ setShowLogin }) => {
-
+  const navigate = useNavigate() ;
   const [menu, setMenu] = useState("home");
-  const {getTotalCartAmount} = useContext(StoreContext)
+  const { getTotalCartAmount, token, setToken } = useContext(StoreContext)
+  const logout = () => {
+    localStorage.removeItem("token");
+    setToken("") ;
+    navigate("/");
+  }
   return (
     <div className='navbar'>
-      <Link to = '/'><img src={assets.logo} alt="" className="logo" /></Link>
+      <Link to='/'><img src={assets.logo} alt="" className="logo" /></Link>
       <ul className="navbar-menu">
         <Link to='/' onClick={() => { setMenu("home") }} className={menu === "home" ? "active" : ""}>Home</Link>
         <a href='#explore-menu' onClick={() => { setMenu("menu") }} className={menu === "menu" ? "active" : ""}>Menu</a>
@@ -22,7 +27,16 @@ const Navbar = ({ setShowLogin }) => {
           <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
           <div className={getTotalCartAmount === 0 ? "" : "dot"}></div>
         </div>
-        <button onClick={() => setShowLogin(true)}>Sign In</button>
+        {!token ? <button onClick={() => setShowLogin(true)}>Sign In</button>
+          : <div className='navbar-profile'>
+            <img src={assets.profile_icon} alt="" />
+            <ul className="nav-profile-dropdown">
+              <li><img src={assets.bag_icon} alt="" /><p>Orders</p></li>
+              <hr />
+              <li onClick={logout}><img src={assets.logout_icon} alt="" /><p>Logout</p></li>
+
+            </ul>
+          </div>}
       </div>
       <hr />
     </div>
--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -1,10 +1,13 @@
 import { createContext, useEffect, useState } from "react";
-import { food_list } from "../assets/frontend_assets/assets";
+import axios from "axios";
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
+    const url = "http://localhost:4000" ;
+    const [token, setToken] = useState("") ;
+    const[food_list,setFoodList] = useState([]);
 
     const addToCart = (itemId) => {
         if (!cartItems[itemId]) {
@@ -28,13 +31,33 @@ const StoreContextProvider = (props) => {
         }
         return totalAmount;
     }
+
+    const fetchFoodList = async() => {
+        const response = await axios.get(url+"/api/food/list") ;
+        console.log(response.data.data);
+        setFoodList(response.data.data) ;
+    }
+
+    useEffect(()=> {
+        async function loadData() {
+            await fetchFoodList() ;
+            console.log("Logged in")
+            if(localStorage.getItem("token")) {
+            setToken(localStorage.getItem("token"));
+            }
+        }
+        loadData();
+    },[])
     const contextValue = {
         food_list,
         cartItems,
         setCartItems,
         addToCart,
         removeFromCart,
-        getTotalCartAmount
+        getTotalCartAmount,
+        url,
+        token,
+        setToken
     }
     return (
         <StoreContext.Provider value={contextValue}>
