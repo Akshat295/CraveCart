833a26651879b2d290082c0c70b7636087dd8bdb
Akshat295 <akshatjohri55@gmail.com>
2026-02-12 16:04:52 +0530

    Added docker to project

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
-    const url = "http://localhost:4000" ;
+    const url = "https://cravecart-uo2h.onrender.com" ;
     const [token, setToken] = useState("") ;
     const [tokenLoading, setTokenLoading] = useState(true);
     const[food_list,setFoodList] = useState([]);
